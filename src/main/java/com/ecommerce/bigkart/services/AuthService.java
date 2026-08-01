6f4fcffac9e996b4b87ba79ad99a6795924c433d
KrishCodesw <krishjain.w@gmail.com>
2026-08-01 11:59:05 +0530

    Creating register endpoint

--- src/main/java/com/ecommerce/bigkart/services/AuthService.java
+++ src/main/java/com/ecommerce/bigkart/services/AuthService.java
@@ -3,9 +3,29 @@ package com.ecommerce.bigkart.services;
 import com.ecommerce.bigkart.dto.AuthResponseDTO;
 import com.ecommerce.bigkart.dto.LoginDTO;
 import com.ecommerce.bigkart.dto.RegisterDTO;
+import com.ecommerce.bigkart.entities.Role;
+import com.ecommerce.bigkart.entities.UserEntity;
+import com.ecommerce.bigkart.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder
+
     public String register(RegisterDTO dto) {
+    if(userRepository.findByUsername(dto.getUsername()).isPresent()){
+        return "Username already exists";
+    }
+        UserEntity user= new UserEntity();
+        user.setUsername(dto.getUsername());
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        Role role=(dto.getRole()!=null && dto.getRole().equalsIgnoreCase("ADMIN")?)
+                user.setRole(role);
 
     }
 
