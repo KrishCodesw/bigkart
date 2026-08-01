1129734bfa50bef326cbcc4521398520cdd702ba
KrishCodesw <krishjain.w@gmail.com>
2026-08-01 12:16:43 +0530

    Login Service

--- src/main/java/com/ecommerce/bigkart/services/AuthService.java
+++ src/main/java/com/ecommerce/bigkart/services/AuthService.java
@@ -7,6 +7,8 @@ import com.ecommerce.bigkart.entities.Role;
 import com.ecommerce.bigkart.entities.UserEntity;
 import com.ecommerce.bigkart.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class AuthService {
@@ -15,7 +17,10 @@ public class AuthService {
     private UserRepository userRepository;
 
     @Autowired
-    private PasswordEncoder passwordEncoder
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     public String register(RegisterDTO dto) {
     if(userRepository.findByUsername(dto.getUsername()).isPresent()){
@@ -24,11 +29,17 @@ public class AuthService {
         UserEntity user= new UserEntity();
         user.setUsername(dto.getUsername());
         user.setPassword(passwordEncoder.encode(dto.getPassword()));
-        Role role=(dto.getRole()!=null && dto.getRole().equalsIgnoreCase("ADMIN")?)
-                user.setRole(role);
+        Role role=(dto.getRole()!=null && dto.getRole().equalsIgnoreCase("ADMIN")?Role.ADMIN:Role.USER);
+        user.setRole(role);
+        userRepository.save(user);
+        return "User registered successfully as "+role;
 
     }
 
     public AuthResponseDTO login(LoginDTO dto) {
+      authenticationManager.authenticate(
+              new UsernamePasswordAuthenticationToken(dto.getUsername(),dto.getPassword())
+      );
+      UserEntity user=userRepository.findByUsername(dto.getUsername()).orElseThrow(()->new RuntimeException("User not found"));
     }
 }
